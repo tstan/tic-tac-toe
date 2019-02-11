e52a50fbebd0657b9540e51ccac3b73f5d35becf
Theodore Tan <theodoresinhantan@gmail.com>
2019-02-10 18:08:46 -0800

    Lift state of squares up to Board and calculate winner

--- src/index.js
+++ src/index.js
@@ -2,34 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import './index.css'
 
-class Square extends React.Component {
-    constructor(props) { // constructor initializes state
-        super(props); // all constructors in React should start with super(props) since they are subclasses of Component
-        this.state = {
-            value: null,
-        };
-    }
-
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => {this.setState({value: 'X'});}} // setState will re-render this when called. It also updates child components!
-            >
-                {this.state.value}
-            </button>
-        );
-    }
+// a function component is kind of like a functional interface, only contains a render method and no state
+function Square(props) { // function component does not extend React.Component...
+    return (
+        // in a function component, don't need to worry about "this" reference
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
+    constructor(props) { // constructor initializes state
+        super(props); // all constructors in React should start with super(props) since they are subclasses of Component
+
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+
+
+    handleClick(i) {
+        const squares = this.state.squares.slice(); // keep squares array immutable
+        if (calculateWinner(squares) || squares[i]) { // ignore click if already filled or game is won
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
     renderSquare(i) {
-        return <Square value={i}/>;
+        return ( // without parentheses, js interpreter will insert a semicolon and break the code
+            <Square
+                value={this.state.squares[i]} // instruct square about its value {X O or null}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
-        const status = 'Next player: X';
-
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
         return (
             <div>
                 <div className="status">{status}</div>
@@ -67,6 +89,26 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 // ========================================
